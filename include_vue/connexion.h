3fa05f913da93dd41d06426a3a06361f5cf807d2
Soumaïla Ramadan <ramadansum@gmail.com>
2016-04-28 12:30:11 +0100

    suppression de l'inclusion de db.h

---
+++ include_vue/connexion.h
@@ -0,0 +1,33 @@
+#ifndef CONNEXION_H
+#define CONNEXION_H
+
+#include <QMainWindow>
+#include "register.h"
+#include "candidatscreen.h"
+
+namespace Ui {
+class Connexion;
+}
+
+class Connexion : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit Connexion(QWidget *parent = 0);
+    ~Connexion();
+
+private slots:
+ void on_register_link_clicked();
+
+ void on_pushButton_clicked();
+
+ void on_btn_connexion_clicked();
+
+private:
+    Ui::Connexion *ui;
+    Register *r = new Register();
+    CandidatScreen *cs = new CandidatScreen();
+};
+
+#endif // CONNEXION_H
