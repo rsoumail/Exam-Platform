fc67963f58359576f84e1a6b004d9c4e968ad30a
Soumaïla Ramadan <ramadansum@gmail.com>
2016-06-20 17:21:35 +0100

    update admin fonctionnality lister les comptes

--- include_vue/adminlistecompteframe.h
+++ include_vue/adminlistecompteframe.h
@@ -2,6 +2,7 @@
 #define ADMINLISTECOMPTEFRAME_H
 
 #include <QFrame>
+#include "creecompteframe.h"
 
 namespace Ui {
 class AdminListeCompteFrame;
@@ -15,8 +16,13 @@ public:
     explicit AdminListeCompteFrame(QWidget *parent = 0);
     ~AdminListeCompteFrame();
 
+    Administrateur getUser() const;
+    void setUser(const Administrateur &value);
+    void lister();
+
 private:
     Ui::AdminListeCompteFrame *ui;
+    Administrateur user;
 };
 
 #endif // ADMINLISTECOMPTEFRAME_H
